5b10a2aa64894d1a37061e7c191bf2eb4c80e92f
Lukas Höppner <lukas.hoeppner@gmail.com>
2021-02-03 19:34:30 +0100

    Add custom font

--- src/App.svelte
+++ src/App.svelte
@@ -7,18 +7,22 @@ import Chat from "./Chat.svelte";
 </main>
 
 <style>
+	@font-face {
+		font-family: Barlow;
+		src: url(/fonts/barlow/Barlow-Medium.ttf);
+	}
+	:global(body) {
+		background-color: black;
+		color: #ef4f57;
+		text-shadow: #ef4f57 0 0 15px;
+		text-transform: uppercase;
+		font-family: Barlow;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
