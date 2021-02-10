74f17e2e1df0e7c4942dafad8c2ec31c73a0e35f
Lukas Höppner <lukas.hoeppner@gmail.com>
2021-02-10 23:11:44 +0100

    Add PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+var cacheName = 'samurai_v1';
+var filesToCache = [
+  '/',
+  '/build/bundle.css',
+  '/build/bundle.js',
+  '/index.html',
+  '/global.css',
+  '/peerjs.min.js',
+  '/sw.js',
+  '/install.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
