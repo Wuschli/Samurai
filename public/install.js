af8122e8760312141fa8adc98ce560a686248935
Lukas Höppner <lukas.hoeppner@gmail.com>
2021-02-11 23:10:21 +0100

    Update cache behaviour

--- public/install.js
+++ public/install.js
@@ -3,6 +3,9 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+      .register('./sw.js')
+      .then((registration) => {
+        setInterval(() => registration.update(), 86400)
+      });
   }
 }
