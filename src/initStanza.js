860616994f87f8588654e3c2f1842f36f6a7eb7a
Lukas Höppner <lukas.hoeppner@gmail.com>
2021-02-25 00:34:43 +0100

    receiving messages

--- src/initStanza.js
+++ src/initStanza.js
@@ -2,8 +2,8 @@ import * as XMPP from 'stanza';
 
 // export const client = 1;
 export const client = XMPP.createClient({
-    jid: 'admin@localhost',
-    password: 'admin',
+    jid: 'Wuschli@localhost',
+    password: '1234',
     transports: {
         websocket: 'ws://localhost:5280/ws',
         bosh: 'https://localhost:5281/http-bind'
@@ -11,15 +11,31 @@ export const client = XMPP.createClient({
 });
 
 client.on('session:started', () => {
+    console.log("XMPP: session:started");
     client.getRoster();
     client.sendPresence();
+    client.enableCarbons();
+    console.log(client);
 });
 
 client.on('chat', msg => {
+    console.log("message received", msg);
     client.sendMessage({
         to: msg.from,
         body: 'You sent: ' + msg.body
     });
 });
+
+client.on('carbon:received', msg => {
+    console.log("carbon received", msg);
+    // client.sendMessage({
+    //     to: msg.from,
+    //     body: 'You sent: ' + msg.body
+    // });
+});
+
+// client.on("*", (event, args) => {
+//     console.log(event, args);
+// });
 
 client.connect();
